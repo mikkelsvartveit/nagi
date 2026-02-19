4f892d31b356056ad9b94bf632dcb7348b3a590b
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2026-02-19 11:31:56 +0100

    Enforce lowercase username

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -29,8 +29,8 @@
     if (!value) return "Username is required";
     if (value.length < 3) return "Username must be at least 3 characters";
     if (value.length > 20) return "Username must be less than 20 characters";
-    if (!/^[a-zA-Z0-9_]+$/.test(value))
-      return "Username can only contain letters, numbers, and underscores";
+    if (!/^[a-z0-9_]+$/.test(value))
+      return "Username can only contain lowercase letters, numbers, and underscores";
     return undefined;
   };
 
